bbd7290d1536a38bfe3bf814a0319e4dc60413f5
sone <sthaya@gmail.com>
2022-03-03 13:36:51 -0800

    test

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/react"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: ["react", "@typescript-eslint"],
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    rules: {
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off"
+    }
+};
