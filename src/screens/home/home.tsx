fd64fee74c31ca22f482616d2cba21d61dd014c8
sone <sthaya@gmail.com>
2022-03-04 09:10:37 -0800

    setup screens

---
+++ src/screens/home/home.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement } from "react";
+import { View, Text, Button } from "react-native";
+import styles from "./home.styles";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackNavigatorParams } from "@config/navigator";
+
+type HomeProps = {
+    navigation: NativeStackNavigationProp<StackNavigatorParams, "Home">;
+};
+
+export default function Home({ navigation }: HomeProps): ReactElement {
+    return (
+        <View style={styles.container}>
+            <Text>Home</Text>
+            <Button
+                title="Game"
+                onPress={() => {
+                    navigation.navigate("Game", { gameId: "455" });
+                }}
+            />
+        </View>
+    );
+}
