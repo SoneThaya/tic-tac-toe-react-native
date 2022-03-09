514b95101645249b8f74425d3d4d6afae9a56177
sone <sthaya@gmail.com>
2022-03-09 09:08:25 -0800

    setup aws amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    api: {
+        tictactoe: {
+            GraphQLAPIKeyOutput: "string";
+            GraphQLAPIIdOutput: "string";
+            GraphQLAPIEndpointOutput: "string";
+        };
+    };
+    auth: {
+        tictactoe72eb5119: {
+            IdentityPoolId: "string";
+            IdentityPoolName: "string";
+            UserPoolId: "string";
+            UserPoolArn: "string";
+            UserPoolName: "string";
+            AppClientIDWeb: "string";
+            AppClientID: "string";
+        };
+    };
+};
